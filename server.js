231159c9747db21af928e0b3ec85833f46ed6bcc
M. Ridho Magribi <magrib1@users.noreply.github.com>
2020-02-04 16:34:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express(),
-    port = process.env.port || 3000;
+    port = process.env.PORT || 3000,
     bodyParser = require('body-parser'),
     controller = require('./controller');
 
@@ -10,7 +10,5 @@ app.use(bodyParser.json());
 var routes = require('./routes');
 routes(app);
 
-app.listen(port, () => {
-      console.log(`Server is running on port ${port}.`);
-    });
+app.listen(port);
 console.log('Learn Node JS With Kiddy, RESTful API server started on: ' + port);

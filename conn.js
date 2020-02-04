cd9ca4c4a405083df1406059b26dc98fd26dd0e8
M. Ridho Magribi <magrib1@users.noreply.github.com>
2020-02-04 15:33:53 +0700

    Update conn.js

--- conn.js
+++ conn.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "172.30.102.92",
-  user: "root",
-  password: "lalin",
-  database: "db_lalin"
+  host: "us-cdbr-iron-east-04.cleardb.net",
+  user: "bdf852b8858eb1",
+  password: "d550138c",
+  database: "heroku_d2bc8e8380b04f5"
 });
 
 con.connect(function (err){

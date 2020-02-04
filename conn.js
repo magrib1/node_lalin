7e45e7b616e8093d038858ded6a3fa8dc7c0d23c
M. Ridho Magribi <magrib1@users.noreply.github.com>
2020-02-04 16:34:01 +0700

    Update conn.js

--- conn.js
+++ conn.js
@@ -1,14 +1,14 @@
-const mysql = require("mysql");
+var mysql = require('mysql');
 
-var con = mysql.createPool({
+var con = mysql.createConnection({
   host: "us-cdbr-iron-east-04.cleardb.net",
   user: "bdf852b8858eb1",
   password: "d550138c",
   database: "heroku_d2bc8e8380b04f5"
 });
 
-// con.connect(function (err){
-//     if(err) throw err;
-// });
+con.connect(function (err){
+    if(err) throw err;
+});
 
 module.exports = con;

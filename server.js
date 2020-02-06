d3c361a91a6717764adf460b67bb99d029a034af
M. Ridho Magribi <magrib1@users.noreply.github.com>
2020-02-06 10:40:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
     bodyParser = require('body-parser'),
     controller = require('./controller');

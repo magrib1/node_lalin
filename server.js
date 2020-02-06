b23dd070d663da384ca965770cf1826c1b96fff2
magrib1 <codex.bar@gmail.com>
2020-02-06 14:50:24 +0700

    aset

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/img'));
 
 var routes = require('./routes');
 routes(app);

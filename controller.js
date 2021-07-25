fcd29d2bc1097cac77b7f8d878a39f013ebd15e7
M. Ridho Magribi <magrib1@users.noreply.github.com>
2021-07-25 23:04:59 +0700

    search cctv,peraturan,rambu

--- controller.js
+++ controller.js
@@ -13,6 +13,21 @@ exports.peraturan = function(req, res) {
     });
 };
 
+exports.findPeraturan = function(req, res) {
+    
+    var id = req.params.id;
+
+    connection.query('select peraturan.pasal_per as pasal_pernya,peraturan.id as idnya,peraturan.isi as isinya,peraturan.ilustrasi as ilunya,pelanggaran.pasal_pel as pasal_pelnya, pelanggaran.denda as dendanya, pelanggaran.pidana as pidananya from peraturan,pelanggaran where peraturan.pasal_per=pelanggaran.pasal_per and peraturan.id = ?',
+    [ id ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            response.ok(rows, res)
+        }
+    });
+};
+
 exports.rambu = function(req, res) {
     connection.query('select no_rambu as nonya,arti as artinya, jenis as jenisnya, gambar as gambarnya from rambu;', function (error, rows, fields){
         if(error){
@@ -23,8 +38,38 @@ exports.rambu = function(req, res) {
     });
 };
 
+exports.findRambu = function(req, res) {
+    
+    var no_rambu = req.params.no_rambu;
+
+    connection.query('select no_rambu as nonya,arti as artinya, jenis as jenisnya, gambar as gambarnya from rambu where no_rambu = ?',
+    [ no_rambu ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            response.ok(rows, res)
+        }
+    });
+};
+
 exports.cctv = function(req, res) {
     connection.query('select * from cctv;', function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            response.ok(rows, res)
+        }
+    });
+};
+
+exports.findCctv = function(req, res) {
+    
+    var urutan_camera = req.params.urutan_camera;
+
+    connection.query('SELECT * FROM cctv where urutan_camera = ?',
+    [ urutan_camera ], 
+    function (error, rows, fields){
         if(error){
             console.log(error)
         } else{
